86ee3fda95ba7e3c3e428307bba09517dd946cee
JBAmbrosio <jblanco@creactibox.com>
2025-02-16 16:40:09 -0600

    cambios en gestion de pagos 2.0

--- custom/addons/gestion_pagos_extension/__manifest__.py
+++ custom/addons/gestion_pagos_extension/__manifest__.py
@@ -1,10 +1,12 @@
 {
     'name': 'gestion de pagos - Extensión',
-    'version': '15.0.1.0.0',
+    'version': '15.0.2.0.0',
     'author': 'SUINER',
     'category': 'Customization',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'views/gestion_pagos_views.xml',
+    ],
     'installable': True,
     'application': False,
 }
--- custom/addons/gestion_pagos_extension/models/gestion_pagos.py
+++ custom/addons/gestion_pagos_extension/models/gestion_pagos.py
@@ -6,14 +6,17 @@ class GestionDePagos(models.Model):
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         """
-        Sobrescribe read_group para agrupar correctamente la suma por moneda.
+        Sobrescribe read_group para agrupar correctamente la suma por moneda y evitar la sumatoria global.
         """
         res = super(GestionDePagos, self).read_group(domain, fields, groupby, offset, limit, orderby, lazy)
 
         if 'x_studio_monto_de_pago' in fields and 'x_studio_moneda' in groupby:
             for line in res:
                 if '__domain' in line:
-                    total = sum(self.search(line['__domain']).mapped('x_studio_monto_de_pago'))
-                    line['x_studio_monto_de_pago'] = total
+                    total_pago = sum(self.env['x_gestion_de_pagos'].search(line['__domain']).mapped('x_studio_monto_de_pago'))
+                    total_aprobado = sum(self.env['x_gestion_de_pagos'].search(line['__domain']).mapped('x_studio_monto_aprobado'))
+
+                    line['x_studio_monto_de_pago'] = total_pago
+                    line['x_studio_monto_aprobado'] = total_aprobado
 
         return res
