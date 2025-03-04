2f1ed2b383311b5a3ca629876f1e5c8a90fae82e
JBAmbrosio <jblanco@creactibox.com>
2025-03-04 13:37:11 -0600

    se agrego dependencia account

--- custom/addons/repositorio_proveedores/__manifest__.py
+++ custom/addons/repositorio_proveedores/__manifest__.py
@@ -2,9 +2,9 @@
     'name': 'Módulo lectura XML',
     'version': '1.0',
     'author': 'MiCompany',
-    'depends': ['base', 'purchase','mail'],  
+    'depends': ['base', 'purchase','account','mail'],  
     'data': [],
-    'installable': True,
+    'installable': True, 
     'application': False, 
     'license': 'LGPL-3',  
 } 
