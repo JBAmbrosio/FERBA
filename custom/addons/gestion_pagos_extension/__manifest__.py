86abeaf0fcca6d5bee5c5a76346838be022125d7
JBAmbrosio <jblanco@creactibox.com>
2026-04-07 15:18:38 -0600

    actualizacion en directorio gestion de pagos

--- custom/addons/gestion_pagos_extension/__manifest__.py
+++ custom/addons/gestion_pagos_extension/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'gestion de pagos',
-    'version': '15.0.2.0.0',
+    'version': '17.0.1.0.0',
     'author': 'Micompany',
     'category': 'Customization',
     'depends': ['x_gestion_de_pagos'],
     'data': [
         'views/gestion_pagos.xml',
     ],
-    'installable': True,
+    'installable': False,
     'application': True, 
 }
