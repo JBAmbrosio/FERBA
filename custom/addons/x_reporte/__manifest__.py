e17237c7a7bac84b0dd1e372a5c0ab7448a0d50a
JBAmbrosio <jblanco@creactibox.com>
2026-07-14 19:45:17 -0600

    Correcciones en archivo manifest

--- custom/addons/x_reporte/__manifest__.py
+++ custom/addons/x_reporte/__manifest__.py
@@ -1,21 +1,21 @@
 {
 'name': 'x_reporte',
 'version': '1.0',
-'author': 'SUINER',
-'category':'sales', 
+'author': 'company',
+'category':'Tools', 
 'summary': 'Gestiona configuraciones globales para los reportes qweb',
-'deppends':[
+'depends':[
 
     'web',
 ] ,
 'data':[
-    'secutity/ir.model.access.csv',
+    'security/ir.model.access.csv',
     'views/view_form_reports_config_custom.xml',
     'views/view_list_reports_config_custom.xml',
 ],
 
 'installable': True,
-'aplication': True,
+'application': True,
 'license': 'LGPL-3',
 
 
