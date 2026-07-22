448dcb2c555ab0bfdc9eeff519d9f89e9e3efca2
JBAmbrosio <jblanco@creactibox.com>
2026-07-22 16:52:58 -0600

    cambio de atributo en class python

--- custom/addons/RFQ/models/x_rfq.py
+++ custom/addons/RFQ/models/x_rfq.py
@@ -3,7 +3,7 @@ from odoo.exceptions import UserError
 
 class RFQ(models.Model):
 
-    _name = "x_rfq"
+    _inherit = "x_rfq"
     _description = "Modelo utilizado para realizar requiciones a inventarios"
   
     x_active = fields.Boolean(string="Activo")
@@ -160,8 +160,8 @@ class RFQ(models.Model):
    
 class RFQLine(models.Model):
 
-    _name="x_rfq_line_f0aac"
-    _description="Modelo que guarda los registros de los productos que se solicitan"
+    _inherit = "x_rfq_line_f0aac"
+    _description = "Modelo que guarda los registros de los productos que se solicitan"
 
     x_name = fields.Char(string="Nombre")
     x_studio_cantidad = fields.Integer(string="Cantidad")
