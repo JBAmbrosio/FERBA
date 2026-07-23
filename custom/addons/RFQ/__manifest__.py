f7cd5291d52ffd341ec6ea207f62988bb0982a13
JBAmbrosio <jblanco@creactibox.com>
2026-07-23 08:04:42 -0600

    pruebas de modelo

--- custom/addons/RFQ/__manifest__.py
+++ custom/addons/RFQ/__manifest__.py
@@ -8,10 +8,12 @@
     'summary': 'El modelo de RFQ permite crear solicitudes a inventario',
     'depends': [
             'web',
+            'stock', 
+            'studio_customization',
           
     ],
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         #'data/ir_sequence.xml',
         #'views/rfq_view_form.xml',  
         #'views/rfq_view_list.xml',
