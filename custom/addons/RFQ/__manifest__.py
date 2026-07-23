be9430af1160067f06aa68e615ba3e1f36495d02
JBAmbrosio <jblanco@creactibox.com>
2026-07-23 12:28:44 -0600

    Actuallización de modelo se inserta en metodo onchangue

--- custom/addons/RFQ/__manifest__.py
+++ custom/addons/RFQ/__manifest__.py
@@ -3,27 +3,20 @@
 
     'name': 'Requisición',
     'version' :'19.0.0.0.0',
-    'author': 'SUINER',
+    'author': 'company',
     'category': 'tools',
     'summary': 'El modelo de RFQ permite crear solicitudes a inventario',
     'depends': [
+            'base',
             'web',
-            'web_studio',
             'stock', 
+            'mrp',
             'studio_customization',
           
     ],
-    'data': [
-        #'security/ir.model.access.csv',
-        'views/rfq_view_form.xml',  
-        'views/rfq_view_list.xml',
-    ],
-
-
-
-
-    'application': True,
-    'installable': True
+    'data': [],
+    'application': False,
+    'installable': True,
 }
 
 
