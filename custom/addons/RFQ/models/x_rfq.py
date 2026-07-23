29351b66e2393b310ce9aa29e867a66eede14728
JBAmbrosio <jblanco@creactibox.com>
2026-07-23 08:20:37 -0600

    Actualización de manifest

--- custom/addons/RFQ/models/x_rfq.py
+++ custom/addons/RFQ/models/x_rfq.py
@@ -2,10 +2,10 @@ from odoo import models, fields
 
 class RFQ(models.Model):
 
-    _name = "x_rfq" 
+    _inherit = "x_rfq" 
     _description = "Modelo utilizado para realizar requiciones a inventarios"
   
-    x_active = fields.Boolean(string="Activo")
+    x_active = fields.Boolean(string="Activo") 
     x_name = fields.Char(string="Nombre")
     materiales_guia = fields.Many2one(string='Material Guía', comodel_name='product.product')
 
