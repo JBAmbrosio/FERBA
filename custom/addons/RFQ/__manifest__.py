bce493d7e6a992b7f0e510c662c25170e4fcd613
JBAmbrosio <jblanco@creactibox.com>
2026-09-07 19:21:04 -0600

    Actualización de manifest RFQ

--- custom/addons/RFQ/__manifest__.py
+++ custom/addons/RFQ/__manifest__.py
@@ -3,7 +3,7 @@
 
     'name': 'Requisición',
     'version' :'19.0.0.0.0',
-    'author': 'SUINER',
+    'author': 'company',
     'category': 'tools',
     'summary': 'El modelo de RFQ permite crear solicitudes a inventario',
     'depends': [
