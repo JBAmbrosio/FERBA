06d2c910ecac5ac45aaa60fa17ad35ccc7b47149
JBAmbrosio <jblanco@creactibox.com>
2026-09-07 18:53:10 -0600

    modulo custom para uso de colores en modulos

---
+++ custom/addons/custom_colors/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Librería Global de Colores',
+    'author': 'SUINER',
+    'version': '19.0.1.0.0',
+    'category': 'Tools',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'custom_colors/static/src/scss/color_palette.scss',
+        ],
+    },
+    'installable': True,
+}
